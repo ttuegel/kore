b835979b05e0d3e890c3938b99af8b49a0912ecd
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-09-20 15:44:41 -0500

    Kore.IndexedModule.IndexedModule: Give context to errors during indexing

--- src/main/haskell/kore/src/Kore/AST/Error.hs
+++ src/main/haskell/kore/src/Kore/AST/Error.hs
@@ -12,12 +12,22 @@ module Kore.AST.Error
     , koreFailWithLocationsWhen
     , withLocationAndContext
     , withLocationsContext
+    , withSentenceAliasContext
+    , withSentenceAxiomContext
+    , withSentenceHookContext
+    , withSentenceImportContext
+    , withSentenceSortContext
+    , withSentenceSymbolContext
+    , withSentenceContext
     ) where
 
 import Data.List
        ( intercalate )
 
 import Kore.AST.AstWithLocation
+import Kore.AST.Common
+import Kore.AST.Kore
+import Kore.AST.Sentence
 import Kore.Error
 
 {-|'koreFailWithLocations' produces an error result with a context containing
@@ -54,10 +64,81 @@ withLocationsContext locations =
 location, to the error context whenever the given action fails.
 -}
 withLocationAndContext
-    :: AstWithLocation astWithLocation
+    :: (AstWithLocation astWithLocation, MonadError (Error e) m)
     => astWithLocation
     -> String
-    -> Either (Error a) result
-    -> Either (Error a) result
+    -> m result
+    -> m result
 withLocationAndContext location message =
     withContext (message ++ " (" ++ prettyPrintLocationFromAst location ++ ")")
+
+withSentenceSymbolContext
+    :: MonadError (Error e) m
+    => KoreSentenceSymbol level
+    -> m a
+    -> m a
+withSentenceSymbolContext
+    SentenceSymbol { sentenceSymbolSymbol = Symbol { symbolConstructor } }
+  =
+    withLocationAndContext symbolConstructor
+        ("symbol '" ++ getId symbolConstructor ++ "' declaration")
+
+withSentenceAliasContext
+    :: KoreSentenceAlias level
+    -> Either (Error e) a
+    -> Either (Error e) a
+withSentenceAliasContext
+    SentenceAlias { sentenceAliasAlias = Alias { aliasConstructor } }
+  =
+    withLocationAndContext aliasConstructor
+        ("alias '" ++ getId aliasConstructor ++ "' declaration")
+
+withSentenceAxiomContext
+    :: KoreSentenceAxiom
+    -> Either (Error e) a
+    -> Either (Error e) a
+withSentenceAxiomContext _ = withContext "axiom declaration"
+
+withSentenceSortContext
+    :: KoreSentenceSort level
+    -> Either (Error e) a
+    -> Either (Error e) a
+withSentenceSortContext
+    SentenceSort { sentenceSortName }
+  =
+    withLocationAndContext sentenceSortName
+        ("sort '" ++ getId sentenceSortName ++ "' declaration")
+
+withSentenceHookContext
+    :: KoreSentenceHook
+    -> Either (Error e) a
+    -> Either (Error e) a
+withSentenceHookContext =
+    \case
+        SentenceHookedSort SentenceSort { sentenceSortName }->
+            withLocationAndContext sentenceSortName
+                ("hooked-sort '" ++ getId sentenceSortName ++ "' declaration")
+        SentenceHookedSymbol SentenceSymbol
+            { sentenceSymbolSymbol = Symbol { symbolConstructor } }
+          ->
+            withLocationAndContext symbolConstructor
+                ("hooked-symbol '" ++ getId symbolConstructor ++ "' declaration")
+
+withSentenceImportContext
+    :: KoreSentenceImport
+    -> Either (Error e) a
+    -> Either (Error e) a
+withSentenceImportContext _ = \go -> go
+
+withSentenceContext
+    :: Sentence level UnifiedSortVariable UnifiedPattern Variable
+    -> Either (Error e) a
+    -> Either (Error e) a
+withSentenceContext =
+    \case
+        SentenceAliasSentence s -> withSentenceAliasContext s
+        SentenceAxiomSentence s -> withSentenceAxiomContext s
+        SentenceHookSentence s -> withSentenceHookContext s
+        SentenceImportSentence s -> withSentenceImportContext s
+        SentenceSortSentence s -> withSentenceSortContext s
+        SentenceSymbolSentence s -> withSentenceSymbolContext s
