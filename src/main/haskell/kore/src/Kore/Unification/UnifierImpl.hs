cfd4285993c49c226e8c48239b6a6edc31de7a2e
Thomas Tuegel <thomas.tuegel@runtimeverification.com>
2018-10-05 15:46:02 -0500

    TODO: normalizeSubstitutionDuplication should be tail-recursive

--- src/main/haskell/kore/src/Kore/Unification/UnifierImpl.hs
+++ src/main/haskell/kore/src/Kore/Unification/UnifierImpl.hs
@@ -39,9 +39,9 @@ import qualified Kore.IndexedModule.MetadataTools as MetadataTools
 import qualified Kore.Predicate.Predicate as Predicate
                  ( isFalse, makeAndPredicate, makeTruePredicate )
 import           Kore.Step.ExpandedPattern
-                 ( ExpandedPattern, PredicateSubstitution(..))
+                 ( ExpandedPattern, PredicateSubstitution (..) )
 import qualified Kore.Step.ExpandedPattern as ExpandedPattern
-                 ( Predicated (..), bottom, top, )
+                 ( Predicated (..), bottom, top )
 import qualified Kore.Step.PredicateSubstitution as PredicateSubstitution
                  ( PredicateSubstitution (..), top )
 import           Kore.Step.StepperAttributes
@@ -228,6 +228,7 @@ normalizeSubstitutionDuplication
         , UnificationProof level variable
         )
 normalizeSubstitutionDuplication tools subst =
+    -- TODO (thomas.tuegel): This can and should be tail-recursive.
     if null nonSingletonSubstitutions
         then return
             ( PredicateSubstitution Predicate.makeTruePredicate subst
