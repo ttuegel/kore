32a2b4abed0c7a640dbdc8af66e4864df86ed089
Thomas Tuegel <ttuegel@mailbox.org>
2019-05-06 16:16:03 -0500

    Illiterate Kore.Step.Simplification.Or
    Few editors and tools (apparently) can handle Literate Haskell files.

--- kore/src/Kore/Step/Simplification/Or.hs
+++ kore/src/Kore/Step/Simplification/Or.hs
@@ -1,17 +1,8 @@
-% Or Simplification
-
-== Header
-
-\begin{code}
-{-|
-Module      : Kore.Step.Simplification.Or
-Description : Tools for Or pattern simplification.
+{- |
 Copyright   : (c) Runtime Verification, 2018
 License     : NCSA
-Maintainer  : virgil.serbanuta@runtimeverification.com
-Stability   : experimental
-Portability : portable
--}
+
+ -}
 module Kore.Step.Simplification.Or
     ( simplifyEvaluated
     , simplify
@@ -21,23 +12,19 @@ import           Control.Applicative
                  ( Alternative (..) )
 import qualified Data.Function as Function
 
-import           Kore.Predicate.Predicate
-                 ( makeOrPredicate )
 import           Kore.Internal.Conditional as Conditional
-import           Kore.Internal.Pattern as Pattern
-import qualified Kore.Internal.OrPattern as OrPattern
+import qualified Kore.Internal.MultiOr as MultiOr
 import           Kore.Internal.OrPattern
                  ( OrPattern )
-import qualified Kore.Internal.MultiOr as MultiOr
+import qualified Kore.Internal.OrPattern as OrPattern
+import           Kore.Internal.Pattern as Pattern
 import           Kore.Internal.TermLike
+import           Kore.Predicate.Predicate
+                 ( makeOrPredicate )
 import           Kore.Syntax.Or
 import           Kore.Unparser
-\end{code}
 
-== Driver
-
-\begin{code}
-{-|'simplify' simplifies an 'Or' pattern with 'OrPattern'
+{- | 'simplify' simplifies an 'Or' pattern with 'OrPattern'
 children by merging the two children.
 -}
 simplify
@@ -48,12 +35,6 @@ simplify
         )
     => Or Sort (OrPattern variable)
     -> OrPattern variable
-\end{code}
-
-`simplify` is a driver responsible for breaking down an `\or` pattern and
-simplifying its children.
-
-\begin{code}
 simplify Or { orFirst = first, orSecond = second } =
     simplifyEvaluated first second
 
@@ -61,6 +42,24 @@ simplify Or { orFirst = first, orSecond = second } =
 
 See 'simplify' for detailed documentation.
 -}
+{- TODO (virgil): Preserve pattern sorts under simplification.
+One way to preserve the required sort annotations is to make `simplifyEvaluated`
+take an argument of type
+
+@
+CofreeF (Or Sort) (Attribute.Pattern variable) (OrPattern variable)
+@
+
+instead of two `OrPattern` arguments. The type of `makeEvaluate` may
+be changed analogously. The `Attribute.Pattern` annotation will eventually cache
+information besides the pattern sort, which will make it even more useful to
+carry around.
+ -}
+
+{- TODO (virgil): This should do all possible mergings, not just the first
+term with the second.
+ -}
+
 simplifyEvaluated
     ::  ( SortedVariable variable
         , Ord variable
@@ -70,23 +69,7 @@ simplifyEvaluated
     => OrPattern variable
     -> OrPattern variable
     -> OrPattern variable
-\end{code}
 
-**TODO** (virgil): Preserve pattern sorts under simplification.
-One way to preserve the required sort annotations is to make `simplifyEvaluated`
-take an argument of type
-``` haskell
-CofreeF (Or Sort) (Attribute.Pattern variable) (OrPattern variable)
-```
-instead of two `OrPattern` arguments. The type of `makeEvaluate` may
-be changed analogously. The `Attribute.Pattern` annotation will eventually cache
-information besides the pattern sort, which will make it even more useful to
-carry around.
-
-**TODO** (virgil): This should do all possible mergings, not just the first term
-with the second.
-
-\begin{code}
 simplifyEvaluated first second
 
   | (head1 : tail1) <- MultiOr.extractPatterns first
@@ -100,11 +83,7 @@ simplifyEvaluated first second
   where
     simplifySinglePatterns first' second' =
         disjoinPredicates first' second' <|> topAnnihilates first' second'
-\end{code}
 
-== Disjoin predicates
-
-\begin{code}
 {- | Merge two configurations by the disjunction of their predicates.
 
 This simplification case is only applied if the configurations have the same
@@ -122,29 +101,35 @@ disjoinPredicates
     -> Pattern variable
     -- ^ Disjunction
     -> Maybe (Pattern variable)
-\end{code}
 
+{-
 When two configurations have the same substitution, it may be possible to
 simplify the pair by disjunction of their predicates.
-```
+
+@
 (             t₁, p₁, s) ∨ (             t₂, p₂, s)
 ([t₂ ∨ ¬t₂] ∧ t₁, p₁, s) ∨ ([t₁ ∨ ¬t₁] ∧ t₂, p₂, s)
 
 (t₁ ∧ t₂, p₁ ∨ p₂, s) ∨ (¬t₂ ∧ t₁, p₁, s) ∨ (¬t₁ ∧ t₂, p₂, s)
-```
+@
+
 It is useful to apply the above equality when
-```
+
+@
 ¬t₂ ∧ t₁ = ¬t₁ ∧ t₂ = ⊥
 t₁ = t₂
-```
+@
+
 so that
-```
+
+@
 (t₁, p₁, s) ∨ (t₂, p₂, s) = (t₁ ∧ t₂, p₁ ∨ p₂, s)
   where
     t₁ = t₂.
-```
+@
+-}
 
-**Note**: [Weakening predicates]
+{- NOTE: [Weakening predicates]
 
 Phillip: It is not clear that we should *ever* apply this simplification.  We
 attempt to refute the conditions on configurations using an external solver to
@@ -165,7 +150,8 @@ be split into many other configurations. Or it may be worse, I don't know.
 To make it short: I agree with this, but I wanted to say the above in case it's
 useful.
 
-\begin{code}
+ -}
+
 disjoinPredicates
     predicated1@Conditional
         { term = term1
@@ -183,21 +169,23 @@ disjoinPredicates
     let result
           | substitution1 == substitution2 =
             predicated1 { predicate = makeOrPredicate predicate1 predicate2 }
-\end{code}
 
+{-
 When the configurations have different substitutions, it is possible to disjoin
 them by promoting the substitutions into the predicate,
-```
+
+@
 (t, p₁     , s₁) ∨ (t, p₂     , s₂)
 (t, p₁ ∧ s₁, ⊤ ) ∨ (t, p₂ ∧ s₂, ⊤ )
-```
+@
+
 and following the algorithm described above for disjoining predicates when the
 substitutions are equal. This is not strictly a simplification: simplifying
 predicates extracts substitutions into the corresponding field of the
 configuration. Nevertheless, this simplification is required by
-`Kore.Step.Simplification.Equals.makeEvaluateFunctionalOr`.
+'Kore.Step.Simplification.Equals.makeEvaluateFunctionalOr'.
+-}
 
-\begin{code}
           | otherwise =
             Conditional
                 { term = term1
@@ -213,11 +201,7 @@ configuration. Nevertheless, this simplification is required by
 
   | otherwise =
     Nothing
-\end{code}
 
-== `\top` annihilates `\or`
-
-\begin{code}
 {- | 'Top' patterns are the annihilator of 'Or'.
  -}
 topAnnihilates
@@ -231,30 +215,35 @@ topAnnihilates
     -> Pattern variable
     -- ^ Disjunction
     -> Maybe (Pattern variable)
-\end{code}
 
+{-
 `⊤` is the annihilator of `∨`; when two configurations have the same
 substitution, it may be possible to use this property to simplify the pair by
 annihilating the lesser term.
-```
+
+@
 (⊤,              p₁, s) ∨ (t₂,              p₂, s)
 (⊤, [p₂ ∨ ¬p₂] ∧ p₁, s) ∨ (t₂, [p₁ ∨ ¬p₁] ∧ p₂, s)
 
 (⊤, p₁ ∧ p₂, s) ∨ (⊤, p₁ ∧ ¬p₂, s) ∨ (t₂, ¬p₁ ∧ p₂, s)
-```
+@
+
 It is useful to apply the above equality when
-```
+
+@
 ¬p₂ ∧ p₁ = ¬p₁ ∧ p₂ = ⊥
 p₁ = p₂
-```
+@
+
 so that
-```
+
+@
 (⊤, p₁, s) ∨ (t₂, p₂, s) = (⊤, p₁ ∧ p₂, s)
   where
     p₁ = p₂.
-```
+@
+-}
 
-\begin{code}
 topAnnihilates
     predicated1@Conditional
         { term = term1
@@ -282,4 +271,3 @@ topAnnihilates
 
   | otherwise =
     Nothing
-\end{code}
