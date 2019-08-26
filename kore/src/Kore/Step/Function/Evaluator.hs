runtimeverification/haskell-backend

Add AxiomIdentifier cases for \exists and \equals (#843)
* GOLD Kore.Step.Axiom.Identifier.matchAxiomIdentifier

* GOLD Test.Kore.Step.Axiom.Identifier

* GREEN Test.Kore.Step.Axiom.Identifier

* RED Test.Kore.Step.Axiom.Identifier

* GREEN Test.Kore.Step.Axiom.Identifier

* GOLD Kore.Step.Axiom.Identifier

* RED Test.Kore.Step.Axiom.Identifier

* GREEN Test.Kore.Step.Axiom.Identifier

* FMT Kore.Step.Simplification.Ceil

kore/src/Kore/Step/Function/Evaluator.hs
@@ -43,7 +43,6 @@ import           Kore.Logger
 import           Kore.Step.Axiom.Identifier
                  ( AxiomIdentifier )
 import qualified Kore.Step.Axiom.Identifier as AxiomIdentifier
-                 ( extract )
 import qualified Kore.Step.Merging.OrPattern as OrPattern
 import           Kore.Step.Simplification.Data as AttemptedAxiom
                  ( AttemptedAxiom (..) )
@@ -195,7 +194,7 @@ maybeEvaluatePattern childrenPredicate termLike defaultValue =
                     attemptResults
   where
     identifier :: Maybe AxiomIdentifier
-    identifier = AxiomIdentifier.extract termLike
+    identifier = AxiomIdentifier.matchAxiomIdentifier termLike
 
     tracing =
         traceMaybeT

ttuegel/kore

kore/src/Kore/Step/Function/Evaluator.hs
@@ -38,7 +38,6 @@ import           Kore.Logger
 import           Kore.Step.Axiom.Identifier
                  ( AxiomIdentifier )
 import qualified Kore.Step.Axiom.Identifier as AxiomIdentifier
-                 ( extract )
 import qualified Kore.Step.Merging.OrPattern as OrPattern
 import           Kore.Step.Simplification.Data as AttemptedAxiom
                  ( AttemptedAxiom (..) )
@@ -227,7 +226,7 @@ maybeEvaluatePattern
                             attemptResults
   where
     identifier :: Maybe AxiomIdentifier
-    identifier = AxiomIdentifier.extract patt
+    identifier = AxiomIdentifier.matchAxiomIdentifier patt
 
     maybeEvaluator :: Maybe BuiltinAndAxiomSimplifier
     maybeEvaluator = do